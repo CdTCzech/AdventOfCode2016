e5a191e05a766b4a2272737a5aff5518e0fb7b6d
Miroslav Hrncir <hrncir@compelson.com>
2016-12-01 10:34:57 +0100

    Initial commit

---
+++ AdventOfCode2016/Source.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <iterator>
+#include <locale>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <vector>
+
+#include "FileReader.h"
+
+typedef unsigned __int64 uint64;
+
